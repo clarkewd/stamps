a1d7a92171d58a7cb5d41024f24e12661a25feaf
Nima Rahnemoon <nrahnemoon@gmail.com>
2014-11-21 15:10:11 -0800

    Set logging.

--- lib/stamps/request.rb
+++ lib/stamps/request.rb
@@ -13,7 +13,7 @@ module Stamps
         globals.endpoint self.endpoint
         globals.namespace self.namespace
         globals.namespaces("xmlns:tns" => self.namespace)
-        globals.log false
+        globals.log true
         globals.logger Logger.new(STDOUT)
         globals.raise_errors false
         globals.headers({ "SoapAction" => formatted_soap_action(web_method) })
